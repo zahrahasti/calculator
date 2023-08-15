cf0bbd1922206f397e8516dd38b1a904ddf7c3cd
zahrahasti <108942575+zahrahasti@users.noreply.github.com>
2023-08-15 18:01:53 +0430

    Refactor code

--- main.js
+++ main.js
@@ -8,82 +8,82 @@ const btnReset=document.querySelector(".btn-reset");
 const btnTheme=document.querySelector(".btn-theme input");
 const box=document.querySelector(".btn-theme span")
 const btnDeleLastNum=document.querySelector(".btn-del")
-
+const btnEqual=document.querySelector(".btn-equel");
 // Function to handle calculator
-const Calculator={
-    x:0,
-    num:0,
-    init:()=>{
-        keyPads.map(key=>{
-            key.addEventListener("click",Calculator.calculate)
-            btnDeleLastNum.addEventListener("click",Calculator.delLastNum)
-            btnReset.addEventListener("click",Calculator.resetResultTest)
-            btnTheme.addEventListener("input",Calculator.changeTheme)
-        })
-    },
-    calculate:function(el){
-        showTextResult.innerText+=el.target.dataset.value
-        const splitResult=showTextResult.textContent.match(/\d+(\.\d+)?|[+*=/-]/g);
-        if(splitResult.length>=4&&splitResult[0]==="-"){
-           splitResult.shift()
-           splitResult[0]=-splitResult[0];
-        }
-    if(splitResult.length>=4)Calculator.checkValue(splitResult,el)
-    if(el.target.dataset.value==="=")Calculator.getResult(splitResult,el)
-  },
-    checkValue:function(splitResult,el){
-        if(splitResult[1]==="+"){
-            Calculator.num=+splitResult[0]+(+splitResult[2])
-            showTextResult.innerText=Calculator.num;
-            showTextResult.innerText+=el.target.dataset.value;
-        }
-          if(splitResult[1]==="*"){
-            console.log('Yes');
-            Calculator.num=+splitResult[0] * (+splitResult[2]);
-            showTextResult.innerText=Calculator.num;
-            showTextResult.innerText+=el.target.dataset.value
-        }else console.log("no");
-          if(splitResult[1]==="-"){
-            Calculator.num=+splitResult[0] - (+splitResult[2]);
-            showTextResult.innerText=Calculator.num;
-            showTextResult.innerText+=el.target.dataset.value
-        }
-          if(splitResult[1]==="/"){
-            Calculator.num=+splitResult[0] / (+splitResult[2])
-            showTextResult.innerText=Calculator.num;
-            showTextResult.innerText+=el.target.dataset.value
-        }
-    },
-     noChangeValue:function(splitResult){if(isNaN(splitResult[2]))showTextResult.innerText=splitResult[0]}
-    ,getResult:function(splitResult,el){
-      Calculator.checkValue(splitResult,el);
-      Calculator.delLastNum();
-      Calculator.noChangeValue(splitResult)
-    },
-    delLastNum:function(){
-      showTextResult.innerText=
-      showTextResult.innerText.slice(0,-1);
-    },
-    resetResultTest:function(){
-      showTextResult.innerText=""
-    }
+// const Calculator={
+//     x:0,
+//     num:0,
+//     init:()=>{
+//         keyPads.map(key=>{
+//             key.addEventListener("click",Calculator.calculate)
+//             btnDeleLastNum.addEventListener("click",Calculator.delLastNum)
+//             btnReset.addEventListener("click",Calculator.resetResultTest)
+//             btnTheme.addEventListener("input",Calculator.changeTheme)
+//         })
+//     },
+//     calculate:function(el){
+//         showTextResult.innerText+=el.target.dataset.value
+//         const splitResult=showTextResult.textContent.match(/\d+(\.\d+)?|[+*=/-]/g);
+//         if(splitResult.length>=4&&splitResult[0]==="-"){
+//            splitResult.shift()
+//            splitResult[0]=-splitResult[0];
+//         }
+//     if(splitResult.length>=4)Calculator.checkValue(splitResult,el)
+//     if(el.target.dataset.value==="=")Calculator.getResult(splitResult,el)
+//   },
+//     checkValue:function(splitResult,el){
+//         if(splitResult[1]==="+"){
+//             Calculator.num=+splitResult[0]+(+splitResult[2])
+//             showTextResult.innerText=Calculator.num;
+//             showTextResult.innerText+=el.target.dataset.value;
+//         }
+//           if(splitResult[1]==="*"){
+//             console.log('Yes');
+//             Calculator.num=+splitResult[0] * (+splitResult[2]);
+//             showTextResult.innerText=Calculator.num;
+//             showTextResult.innerText+=el.target.dataset.value
+//         }else console.log("no");
+//           if(splitResult[1]==="-"){
+//             Calculator.num=+splitResult[0] - (+splitResult[2]);
+//             showTextResult.innerText=Calculator.num;
+//             showTextResult.innerText+=el.target.dataset.value
+//         }
+//           if(splitResult[1]==="/"){
+//             Calculator.num=+splitResult[0] / (+splitResult[2])
+//             showTextResult.innerText=Calculator.num;
+//             showTextResult.innerText+=el.target.dataset.value
+//         }
+//     },
+//      noChangeValue:function(splitResult){if(isNaN(splitResult[2]))showTextResult.innerText=splitResult[0]}
+//     ,getResult:function(splitResult,el){
+//       Calculator.checkValue(splitResult,el);
+//       Calculator.delLastNum();
+//       Calculator.noChangeValue(splitResult)
+//     },
+//     delLastNum:function(){
+//       showTextResult.innerText=
+//       showTextResult.innerText.slice(0,-1);
+//     },
+//     resetResultTest:function(){
+//       showTextResult.innerText=""
+//     }
 
-   ,changeTheme:function(e){
-    document.body.classList.remove("style-1","style-2","style-3");
-     if(e.target.value===this.min)
-     document.body.classList.add("style-1")
+//    ,changeTheme:function(e){
+//     document.body.classList.remove("style-1","style-2","style-3");
+//      if(e.target.value===this.min)
+//      document.body.classList.add("style-1")
 
-     if(+e.target.value===+this.min + (+ this.step))
-     document.body.classList.add("style-2")
+//      if(+e.target.value===+this.min + (+ this.step))
+//      document.body.classList.add("style-2")
 
-     if(e.target.value===this.max)
-     document.body.classList.add("style-3")
-   }
+//      if(e.target.value===this.max)
+//      document.body.classList.add("style-3")
+//    }
    
-}
+// }
 
-// call the main function 
-Calculator.init();
+// // call the main function 
+// Calculator.init();
  
 
 // Function to handle key press events
@@ -96,3 +96,47 @@ Calculator.init();
 
 // // Attach key press event listener to the entire document
 // document.addEventListener("keypress", handleKeyPress);
+
+// todo second way to calc
+let string;
+let nums=[];
+keyPads.map(key=>{
+  key.addEventListener("click",checkValue)
+})
+btnEqual.addEventListener("click",()=>calc(string));
+btnReset.addEventListener("click",resetText);
+btnDeleLastNum.addEventListener("click",delLastCharacter)
+
+
+function delLastCharacter(){
+  showTextResult.textContent=showTextResult.textContent.slice(0,-1);
+  nums.pop()
+  string=string.slice(0,-1)
+}
+
+function resetText(){
+  nums=[];
+  showTextResult.textContent="";
+  string="";
+}
+
+function checkValue(e){
+    const value=e.target.dataset.value;
+    showTextResult.textContent+=value;
+    nums.push(value);
+   string=nums.join("");
+  
+}
+
+
+function calc(string){
+        if(isNaN(string.at(-1))){
+
+        }
+        else{
+        showTextResult.textContent=`${eval(string)}`;
+        string=showTextResult.textContent;
+        nums=[];
+        nums.push(string);
+}
+}
