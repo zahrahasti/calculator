00c58d8d3b1978f30e55ba9a99c25c011e5f26a8
zahrahasti <hastizahra984@gmail.com>
2024-05-17 13:40:41 +0330

    ignore ts in calulator.ts file and fixed some bugs

--- src/assets/ts/modules/calculate.ts
+++ src/assets/ts/modules/calculate.ts
@@ -4,12 +4,12 @@ import {errorText} from "@modules/errorText";
 import { DomElements } from "@ts/domEl";
 let {showTextResult,calculationComponents}=DomElements
 function calculate(txt:string) {
-    console.log("texty",txt);
+    console.log(txt);
     if(eval(removeLeadingZeros(`${txt}`))===Infinity){
       errorText("Can`t divide by zero");
       return;
     }
-    
+   
     if (isNaN(txt.at(-1))) errorText("Invalid format used")
      else {
       showTextResult.textContent = `${eval(removeLeadingZeros(`${txt}`))}`;
