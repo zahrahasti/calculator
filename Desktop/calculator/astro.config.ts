3510291444e395c5cb3ee8d0592c9095ab6cf0a7
zahrahasti <hastizahra984@gmail.com>
2025-10-23 17:39:38 +0330

    update code

--- Desktop/calculator/astro.config.ts
+++ Desktop/calculator/astro.config.ts
@@ -1,27 +1,23 @@
-import icon from "astro-icon";
-import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  integrations: [
-    icon({
-      include: {
-        lucide: ["plus", "minus", "x", "divide", "delete", "trash-2"],
-      },
-    }),
-  ],
-  base: "calculator",
-  trailingSlash: "ignore",
-  build: {
-    format: "file",
-    assets: "assets",
-  },
-  server: {
-    port: 3000,
-    host: "127.0.0.1",
-    open: true,
-  },
-  vite: {
-    plugins: [tailwindcss()],
-  },
+	integrations: [
+		icon(),
+	],
+	base: 'calculator',
+	trailingSlash: 'ignore',
+	build: {
+		format: 'file',
+		assets: 'assets',
+	},
+	server: {
+		port: 3000,
+		host: '127.0.0.1',
+		open: true,
+	},
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
